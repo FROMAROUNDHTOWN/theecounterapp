448d2298cb571f7664361af24e0c90089f2eb3ee
FROMAROUNDHTOWN <kweenyoallhiada@gmail.com>
2021-03-15 16:39:46 -0700

    Initialize thee counter app.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// YOU GOT THIS SIS! 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
+reportWebVitals();
